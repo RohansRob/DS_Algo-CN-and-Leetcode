b02fb42d5c2b74f2c24003ed1bba2165c5b1e16d
Rohan <106212014+RohansRob@users.noreply.github.com>
2022-09-27 08:26:22 +0530

    Add files via upload

---
+++ Python3/CheckFor+ve-ve.py
@@ -0,0 +1,10 @@
+#Check Number +ve or -ve 
+
+n = int(input())
+if n == 0:
+    print('zero')
+else :
+    if n > 0 :
+        print('positive')
+    else :
+        print('Negative')
---
+++ Python3/SumOfnNumbers.py
@@ -0,0 +1,8 @@
+# Sum of n numbers
+
+n = int(input())
+sum =1
+while sum<n :
+    sum = (n/2)*(n+1)
+    sum = int(sum)
+    print(sum)
