7e2529476c32f799d20829f3a1f5e7d6053f985a
Rohan <106212014+RohansRob@users.noreply.github.com>
2022-09-29 08:26:02 +0530

    Add files via upload

--- Recursion/FunctionCall1.py
+++ Recursion/FunctionCall1.py
@@ -9,6 +9,12 @@ def fact(n):
 #     SmallOuput = fact(n-1)
 #     return n * SmallOutput     
 #
+def sum_n(n):
+    if n == 0
+        return 0
+    smallOutput = sum_n(n-1)
+    output = smallOutput + n
+    return output
 
 n = int(input())
 fact(n)
